e7e669b48138886effed8a4a1c31939b7703144b
George Hilliard <thirtythreeforty@lavabit.com>
2013-08-27 14:43:35 -0500

    Minor optimizations and cleanups for CRC16 code

--- src/00/crypto.asm
+++ src/00/crypto.asm
@@ -8,26 +8,25 @@
 crc16:
     push hl
     push bc
-    ld de, 0xFFFF                    ; initialize crc
+    ld de, 0xFFFF                        ; initialize crc
 .bytelp:
         push bc                          ; save count
             ld a, (hl)                   ; fetch byte from memory
-            ; the following code updates the crc in de with the byte in a
             xor d                        ; xor byte into crc top byte
             ld b, 8                      ; prepare to rotate 8 bits
 .rotlp:
             sla e
             adc a, a                     ; rotate crc
-            jp nc, _                     ; b15 was zero
+            jr nc, _                     ; b15 was zero
             ld d, a                      ; put crc high byte back into d
             ld a, e \ xor 0x21 \ ld e, a ; crc=crc xor &1021, xmodem polynomic
             ld a, d \ xor 0x10           ; and get crc top byte back into a
-_:          dec b \ jr nz, .rotlp        ; loop for 8 bits
+_:          djnz .rotlp
             ld d, a                      ; put crc top byte back into d
 
-            inc hl                       ; step to next byte
+            inc hl
         pop bc
-        dec bc                           ; num=num-1
+        dec bc
         ld a, b \ or c
         jr nz, .bytelp                   ; loop until num=0
     pop bc
