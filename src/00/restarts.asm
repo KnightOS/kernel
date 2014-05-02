5eac3edcc4e7205c343a275288072acf2ff38c22
matrefeytontias <mattias@refeyton.fr>
2014-05-02 13:52:53 +0200

    Added page restoring to and optimized pcall

--- src/00/restarts.asm
+++ src/00/restarts.asm
@@ -132,29 +132,37 @@ pcall:
     ; Stack state : calling addr
     push af
         ; Stack state : calling addr, original AF
-        ld a, i
-        jp po, .pcall_noInt
-        ld a, i
-        jp po, .pcall_noInt
-        di
-        push hl
-            ; Stack state : calling addr, original AF, original HL
-            ld hl, .returnPoint
-        pop af
-        ; Stack state : calling addr, original AF #### AF contains original HL
-        ex (sp), hl
-        ; Stack state : calling addr, .returnPoint #### AF contains original HL, HL contains original AF
+        getBankA
         push af
-            ; Stack state : calling addr, .returnPoint, original HL
-            ex (sp), hl
-            ; Stack state : calling addr, .returnPoint, original AF
-            ; also, no registers are stashed - excepting AF, but that's part of the plan
-            ; and since we can destroy AF, we do that instead of inc sp \ inc sp
+            ; Stack state : calling addr, original AF, page
+            ld a, i
+            ld a, i
+            push af
+                ; Stack state : calling addr, original AF, page, interrupt state
+                di
+                ex (sp), hl
+                ; Stack state : calling addr, original AF, page, original HL #### HL = interrupt state
+            inc sp \ inc sp
+        inc sp \ inc sp
+        ex (sp), hl
+        ; Stack state : calling addr, interrupt state, page, original HL #### HL = original AF
+            dec sp \ dec sp
+                dec sp \dec sp
+                push hl \ pop af
+                ld hl, .returnPoint
+                ex (sp), hl
+                ; Stack state : calling addr, interrupt state, page, return point
+                push af
+                    ; Stack state ; calling addr, interrupt state, page, return point, original AF
+                    ; now the fun really starts, since we can trash AF as much as we want
+                    ; avoid inc sp \ inc sp by precisely trashing AF
+                pop af
+            pop af
         pop af
     pop af
-    ; SP is on calling addr
+    ; SP points on calling addr
     ex (sp), hl
-    ; Stack state : original HL, .returnPoint, original AF
+    ; Stack state ; original HL, interrupt state, page, return point, original AF #### HL contains calling addr
 #ifdef FLASH4MB
     xor a
     out (0x0E), a
@@ -168,78 +176,49 @@ pcall:
     ld a, (hl)
     inc hl
     ex (sp), hl
-    ; Stack state : calling addr + 2 (ret addr), .returnPoint, original AF
+    ; Stack state ; calling addr + 2 (ret addr), interrupt state, page, return point, original AF
+    ; SP still points on ret addr
     dec sp \ dec sp
         dec sp \ dec sp
-            ; SP is on original AF
-            push hl
-                ; Stack state : ret addr, .returnPoint, original AF, original HL
-                inc a
-                ld l, a
-                xor a
-                ld h, 0x7F
-                sub l
-                sub l \ jr nc, $ + 3 \ dec h
-                sub l \ jr nc, $ + 3 \ dec h
-                ld l, a
-            pop af
-            ; Stack state : ret addr, .returnPoint, original AF #### AF contains original HL
-            ex (sp), hl
-            ; Stack state : ret addr, .returnPoint, pcall addr #### AF contains original HL, HL contains original AF
-            push af
-            ; Stack state : ret addr, .returnPoint, pcall addr, original HL #### HL contains original AF
-                ex (sp), hl
-                ; Stack state : ret addr, .returnPoint, pcall addr, original AF
-            pop af
-            ; Stack state : ret addr, .returnPoint, pcall addr
-            ret
+            dec sp \ dec sp
+                dec sp \ dec sp
+                    ; SP is on original AF
+                    push hl
+                        ; Stack state ; calling addr + 2 (ret addr), interrupt state, page, return point, original AF, original HL
+                        ; Does HL = 0x8000 - (a + 1) * 3
+                        inc a
+                        ld l, a
+                        xor a
+                        ld h, 0x7f
+                        sub l
+                        sub l \ jr nc, $ + 3 \ dec h
+                        sub l \ jr nc, $ + 3 \ dec h
+                        ld l, a
+                    pop af
+                    ; Stack state ; calling addr + 2 (ret addr), interrupt state, page, return point, original AF #### AF contains original HL
+                    ex (sp), hl
+                    ; Stack state ; calling addr + 2 (ret addr), interrupt state, page, return point, pcall addr #### AF contains original HL, HL contains original AF
+                    ; gives back registers their respective values
+                    push af
+                        ex (sp), hl
+                    pop af
+                    ; Stack state ; calling addr + 2 (ret addr), interrupt state, page, return point, pcall addr
+                    ret
 .returnPoint:
+    ; Stack state : ret address, interrupt state, page
+#ifdef FLASH4MB
+            xor a
+            out (0x0E), A
+        pop af
+        out (0x06), a
+#else
+        pop af
+        setBankA
+#endif
+    pop af
+    ret po
     ei
     ret
-    
-.pcall_noInt:
-        ; Stack state : calling addr, original AF
-    pop af
-    ; SP is on calling addr
-    ex (sp), hl
-    ; Stack state : original HL, original AF
-#ifdef FLASH4MB
-    xor a
-    out (0x0E), A
-    ld a, (hl)
-    out (0x06), a
-#else
-    ld a, (hl)
-    setBankA
-#endif
-    inc hl
-    ld a, (hl)
-    inc hl
-    ex (sp), hl
-    ; Stack state : calling addr + 2 (ret addr), original AF
-        dec sp \ dec sp
-        ; SP is on original AF
-        push hl
-            ; Stack state : ret addr, original AF, original HL
-            inc a
-            ld l, a
-            xor a
-            ld h, 0x7F
-            sub l
-            sub l \ jr nc, $ + 3 \ dec h
-            sub l \ jr nc, $ + 3 \ dec h
-            ld l, a
-        pop af
-        ; Stack state : ret addr, original AF #### AF contains original HL
-        ex (sp), hl
-        ; Stack state : ret addr, pcall addr #### AF contains original HL, HL contains original AF
-        push af
-            ; Stack state : ret addr, pcall addr, original HL #### HL contains original AF
-            ex (sp), hl
-            ; Stack state : ret addr, pcall addr, original AF
-        pop af
-        ; Stack state : ret addr, pcall addr
-        ret ; jump to the actual pcall
 
 ; rst $28
 bcall:
