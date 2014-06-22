87faee26e0268a3892bcb38a5774d89ed9b1af7e
George Hilliard <thirtythreeforty@lavabit.com>
2014-06-22 01:19:00 -0500

    Implement initial version of condition variables

--- src/00/concurrency.asm
+++ src/00/concurrency.asm
@@ -42,3 +42,120 @@ initMutex:
 releaseMutex:
     ld (hl), 0
     ret
+
+;; condInit [Concurrency]
+;;  Returns a pointer to a newly allocated condition variable.
+;;  The application must [[free]] this variable when no threads
+;;  are using it any longer.
+;; Outputs:
+;;  Z: Set on success, reset on failure
+;;  A: Error code (on failure)
+;;  IX: Pointer to condition variable (on success)
+condInit:
+    push af
+    push bc
+        ; Up to maxThreads-1 may wait.  (All threads waiting would never awaken.)
+        ; The extra byte is the counter.
+        ld bc, maxThreads
+        call malloc
+        jr nz, .fail
+        xor a
+        call memset
+    pop bc
+    pop af
+    cp a
+    ret
+.fail:
+    pop bc
+    inc sp \ inc sp ;pop af
+    ret
+
+;; condWait [Concurrency]
+;;  Blocks execution of the current thread until another thread
+;;  notifies the condition variable.  A thread that intends to
+;;  wait on a condition variable must first acquire a mutex and
+;;  pass it to this routine.  The wait operation will atomically
+;;  release the mutex and suspend the thread.  When the condition
+;;  variable is notified, the thread is reawakened and the mutex
+;;  is reacquired.  Interrupts will be enabled by this routine.
+;; Inputs:
+;;  HL: Pointer to locked mutex
+;;  IX: Pointer to condition variable
+condWait:
+    push af
+    push ix
+        push de
+            inc (ix)
+            ld e, (ix)
+            ld d, 0
+            add ix, de
+        pop de
+        call getCurrentThreadID
+        ld (ix), a
+        call releaseMutex
+        call suspendCurrentThread
+        call acquireMutex
+_:  pop ix
+    pop af
+    ret
+
+;; condNotifyAll [Concurrency]
+;;  Awakens all threads waiting on a condition variable.
+;;  The threads should check to see if the condition that they were
+;;  waiting for (perhaps if a queue has data) is still true!
+;;  To avoid corner-case race conditions, the calling thread must
+;;  lock the associated mutex before invoking this routine (and
+;;  release it when done).
+;; Inputs:
+;;  IX: Pointer to condition variable
+condNotifyAll:
+    push af
+    push bc
+    push ix
+        ld b, (ix)
+_:      inc ix
+        ld a, (ix)
+        call resumeThread
+        djnz -_
+    pop ix
+    ld (ix), 0
+    pop bc
+    pop af
+    ret
+
+;; condNotifyOne [Concurrency]
+;;  Awakens the thread that has been waiting the longest on a
+;;  condition variable.
+;;  The threads should check to see if the condition that they were
+;;  waiting for (perhaps if a queue has data) is still true!
+;;  To avoid corner-case race conditions, the calling thread must
+;;  lock the associated mutex before invoking this routine (and
+;;  release it when done).
+;; Inputs:
+;;  IX: Pointer to condition variable
+condNotifyOne:
+    push af
+    push bc
+    push hl
+        push ix \ pop hl
+        xor a
+        or (hl)
+        jr z, .done
+        dec (hl)
+        ld b, (hl)
+        inc hl
+        ld a, (hl)
+        call resumeThread
+        xor a
+        or b
+        jr z, .done
+        push de
+            push hl \ pop de
+            inc hl
+            ldir
+        pop de
+.done:
+    pop hl
+    pop bc
+    pop af
+    ret
