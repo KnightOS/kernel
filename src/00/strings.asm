32916f5107216723d29de87f687294b0af474e25
matrefeytontias <mattias@refeyton.fr>
2014-09-02 19:11:16 +0200

    Made strtoi more modulable ; also, it now converts 32-bits numbers

--- src/00/strings.asm
+++ src/00/strings.asm
@@ -106,19 +106,24 @@ strchr:
     ret
     
 ;; strtoi [Strings]
-;;  Converts an ASCII-encoded unsigned decimal into a word.
+;;  Converts an ASCII-encoded unsigned decimal into a word of variable size.
 ;; Inputs:
 ;;  HL: pointer on ASCII-encoded decimal
+;;  B: maximum number of digits to convert
 ;; Outputs:
-;;  DE: converted byte
+;;  DEHL: converted word
 ;;  Z: set on success
 ;; Notes:
-;;  The routine will scan 5-digits numbers that are over 65535 
-;; as valid numbers, even if HL cannot contain said numbers.
+;;  The routine will ignore leading zeroes to produce a number composed by a 
+;;  maximum of 10 digits, the maximal value being 4,294,967,295. If a 10-digits
+;;  number with a greater value is encountered, no error will be thrown but the
+;;  number won't be converted as expected.
+;;  Destroys BC', DE' and HL'.
 strtoi:
-    push hl \ push bc
+    push bc
         push hl
-            ld b, 0
+            dec b
+            ld c, 0
 .countDigits:
             ld a, (hl)
             cp '0'
@@ -126,50 +131,81 @@ strtoi:
             cp '9' + 1
             jr nc, .noMoreDigits
             inc hl
-            inc b
-            ld a, 4
-            cp b
+            inc c
+            ld a, 9 ; maximum of 10 digits
+            cp c
+            jr c, .noMoreDigits
+            ld a, b
+            cp c
             jr nc, .countDigits
 .noMoreDigits:
             xor a
-            cp b
+            cp c
         pop hl
         jr z, .error
         ; HL = string
         ; B = number of digits
-        ; for(; B > 0 ; HL++, B--)
-        ;   num += (*HL - '0') * miniLUT[B - 1];
-        ld c, 0
+        ; for(; B >= 0 ; string++, B--)
+        ;   DEHL += (*string - '0') * miniLUT[B - 1];
+        exx
+        ld de, 0
+        ld hl, 0
+        exx
+        ld b, c
+.formWordLoop:
+        ld a, (hl)
+        sub '0'
         push hl
-            ld hl, 0
-            ex (sp), hl
-.formByteLoop:
-            ld a, (hl)
-            sub '0'
-            ex (sp), hl
-            push hl
-                ld hl, .factorsLUT
-                ld e, b
-                ld d, 0
-                dec e
-                add hl, de
-                add hl, de
-                ld e, (hl)
-                inc hl
-                ld d, (hl)
-                call mul16By8
-            pop de
+            ld l, b
+            ld h, 0
+            dec l
+            add hl, hl
+            add hl, hl
+            ld de, .factorsLUT + 3
             add hl, de
-            ex (sp), hl
-            inc hl
-            djnz .formByteLoop
-        pop de
+            ld d, (hl)
+            dec hl
+            ld e, (hl)
+            dec hl
+            ld c, (hl)
+            dec hl
+            ld l, (hl)
+            ld h, c
+            call mul32By8
+            push de
+                push hl
+                    exx
+                pop bc
+                add hl, bc
+                ex de, hl
+            pop bc
+            adc hl, bc
+            ex de, hl
+            exx
+        pop hl
+        inc hl
+        djnz .formWordLoop
+        exx
+        push de \ push hl
+            exx
+        pop hl \ pop de
         xor a
         jr .error + 2
 .error:
         xor a
         inc a
-    pop bc \ pop hl
+    pop bc
     ret
+    
+    ; 4-bytes factors
 .factorsLUT:
-    .dw 1, 10, 100, 1000, 10000
+    .dw 1, 0
+    .dw 10, 0
+    .dw 100, 0
+    .dw 1000, 0
+    .dw 10000, 0
+    .dw $86a0, $0001    ; 100,000
+    .dw $4240, $000f    ; 1,000,000
+    .dw $9680, $0098    ; 10,000,000
+    .dw $e100, $05f5    ; 100,000,000
+    .dw $ca00, $3b9a    ; 1,000,000,000
